93440c10ae1675c517ffb0cdaddeef04582806c9
Naufal Zaky <naufalzaky754@gmail.com>
2022-12-28 14:16:57 +0700

    Push Helper
    Push Helper

---
+++ api/test/helper/fileHelper.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const fileName = path.join(__dirname, '..', "/data/data.json");
+
+function readFile() {
+  try {
+    const fileContent = fs.readFileSync(fileName, "utf8");
+    const jsonData = JSON.parse(fileContent);
+    return {
+      success: true,
+      data: jsonData,
+    };
+  } catch (e) {
+    return {
+      success: false,
+      data: null,
+    };
+  }
+}
+
+function writeFile(key, data) {
+  try {
+    const fileContent = fs.readFileSync(fileName, "utf8");
+    const jsonData = JSON.parse(fileContent);
+    jsonData[key] = data;
+
+    const stringifiedContent = JSON.stringify(jsonData);
+    fs.writeFileSync(fileName, stringifiedContent);
+  } catch (e) {
+    // Do nothing
+  }
+}
+
+function getRandomNumber() {
+  const randomNumber = Math.floor((Math.random() * 1000000) + 1);
+  return randomNumber;
+}
+
+module.exports = { readFile, writeFile, getRandomNumber };
